51da631860e6fc62df17e052ef8b166ca2ceceb5
Jeremy Xu <xinjiexu@tencent.com>
2019-07-02 10:02:31 +0800

    make refactor

--- backend/etcd/backend.go
+++ backend/etcd/backend.go
@@ -20,13 +20,11 @@ import (
 	"errors"
 	"net"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/jeremyxu2010/cni-ipam-etcd/backend/allocator"
 
 	"github.com/coreos/etcd/clientv3"
-	"github.com/coreos/etcd/pkg/transport"
 	"github.com/jeremyxu2010/cni-ipam-etcd/backend"
 )
 
@@ -47,8 +45,8 @@ func New(name string, ipamConfig *allocator.IPAMConfig) (*Store, error) {
 	if err != nil {
 		panic(err)
 	}
-	network, err := netConfigJson(ipamConfig)
-	etcdKeyPrefix, err := initStore(name, network, etcdClient)
+	netConfig, err := netConfigJson(ipamConfig)
+	etcdKeyPrefix, err := initStore(name, netConfig, etcdClient)
 	// write values in Store object
 	store := &Store{
 		EtcdClient:    etcdClient,
@@ -57,10 +55,10 @@ func New(name string, ipamConfig *allocator.IPAMConfig) (*Store, error) {
 	return store, nil
 }
 
-func initStore(name string, network string, etcdClient *clientv3.Client) (string, error) {
+func initStore(name string, netConfig string, etcdClient *clientv3.Client) (string, error) {
 	key := ETCDPrefix + name
 
-	_, err := etcdClient.Put(context.TODO(), key, network)
+	_, err := etcdClient.Put(context.TODO(), key, netConfig)
 	if err != nil {
 		panic(err)
 	}
@@ -70,52 +68,6 @@ func initStore(name string, network string, etcdClient *clientv3.Client) (string
 func netConfigJson(ipamConfig *allocator.IPAMConfig) (string, error) {
 	conf, err := json.Marshal(ipamConfig.Ranges)
 	return string(conf), err
-}
-
-func connectStore(etcdConfig *allocator.EtcdConfig) (*clientv3.Client, error) {
-
-	var etcdClient *clientv3.Client
-	var err error
-	if strings.HasPrefix(etcdConfig.EtcdURL, "https") {
-		etcdClient, err = connectWithTLS(etcdConfig.EtcdURL, etcdConfig.EtcdCertFile, etcdConfig.EtcdKeyFile, etcdConfig.EtcdTrustedCAFileFile)
-	} else {
-		etcdClient, err = connectWithoutTLS(etcdConfig.EtcdURL)
-	}
-
-	return etcdClient, err
-}
-
-/*
-	ETCD Related
-*/
-func connectWithoutTLS(url string) (*clientv3.Client, error) {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{url},
-		DialTimeout: 5 * time.Second,
-	})
-
-	return cli, err
-}
-
-func connectWithTLS(url, cert, key, trusted string) (*clientv3.Client, error) {
-	tlsInfo := transport.TLSInfo{
-		CertFile:      cert,
-		KeyFile:       key,
-		TrustedCAFile: trusted,
-	}
-
-	tlsConfig, err := tlsInfo.ClientConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{url},
-		DialTimeout: 5 * time.Second,
-		TLS:         tlsConfig,
-	})
-
-	return cli, err
 }
 
 func (s *Store) Reserve(id string, ip net.IP, rangeID string) (bool, error) {
